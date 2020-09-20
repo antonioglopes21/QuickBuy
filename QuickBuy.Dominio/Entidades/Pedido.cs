9d689fb0a9a8f74da2ce1902f538eed5f1ea9d57
Marcos Lopes <antonioglopes21@hotmail.com>
2020-09-20 17:52:21 -0300

    Seção 4 ate 6

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,14 +1,16 @@
 ﻿using QuickBuy.Dominio.ObjetoDeValor;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Pedido
+    public class Pedido: Entidade
     {
         public int Id { get; set; }
         public DateTime DatPedido { get; set; }
         public int UsuarioId { get; set; }
+        public virtual Usuario Usuario { get; set; }
         public DateTime DataPrevisaoEntrega { get; set; }
         public string CEP { get; set; }
         public string Estado { get; set; }
@@ -17,8 +19,22 @@ namespace QuickBuy.Dominio.Entidades
         public int NumeroEndereco { get; set; }
 
         public int FormaPagamentoId { get; set; }
-        public FormaPagamento FormaPagamento { get; set; }
+        public virtual FormaPagamento FormaPagamento { get; set; }
 
-        public ICollection<ItemPedido> ItensPedidos { get; set; }
+        public virtual ICollection<ItemPedido> ItensPedidos { get; set; }
+
+        public override void Validate()
+        {
+            LimparMensagemValidacao();
+
+            if (!ItensPedidos.Any())
+                AdicionarCritica("Crítica -  Pedido não pode ficar sem item de pedido");
+            
+            if(string.IsNullOrEmpty(CEP))
+                AdicionarCritica("Crítica -  CEP deve estar preenchido");
+
+            if (FormaPagamentoId == 0)
+                AdicionarCritica("Crítica - Não foi informado a forma de pagamento");
+        }
     }
 }
